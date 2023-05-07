6190eb64e2e9275918a3cea529342a34fe9aaf90
unknown <gubaev1998ildarick@mail.ru>
2023-05-08 00:35:17 +0300

    first commit

---
+++ src/app/site-layout/site-layout.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SiteLayoutComponent } from './site-layout.component';
+
+
+
+@NgModule({
+  declarations: [
+    SiteLayoutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SiteLayoutModule { }
